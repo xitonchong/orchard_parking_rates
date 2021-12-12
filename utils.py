cd2dea1eda63243edfe17e713deed7cef4e7d335
hentai_usagi <xitonchong@gmail.com>
2021-12-12 21:18:06 +0800

    added generate_combinations(), get_hour() to utils

--- utils.py
+++ utils.py
@@ -2,6 +2,18 @@
 
 import re 
 import time 
+from itertools import product 
+from typing import List
+
+
+
+def get_hour(t, output='hour'):
+    h, m =map(int, time.strftime("%H %M", t).split(' '))
+    if output == 'hour':
+        if m >= 59:
+            h += 1
+        return h
+    return h, m 
 
 
 def convert_to_datetime(mystr):
@@ -18,6 +30,12 @@ def split_lines(text):
     lines = re.split('(?<=\.)\s|;\s+', text)
     return lines
 
+def generate_combinations(a: List[list]):
+    ''' a: [[1,2], [3,4], [5,6]]
+        returns: all cominations
+    '''
+    return list(product(*a))
+
 
 def find_numerical_inputs(line):
     # (?:[eE][+-]?\d+)? optional 1e2343
@@ -28,3 +46,4 @@ if __name__ == '__main__':
     pass # moved to unit test
     # res = convert_to_datetime("5am")
     # assert time.strftime("%H:%M:%S", res) == '05:00:00',  print('error in time conversion')
+    # hour, minute = map(int, time.strftime("%H,%M", res).split(','))
